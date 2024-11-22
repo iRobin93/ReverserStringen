b117b272c70d9b60310ae94bb5bd936da4de6085
iRobin93 <61976533+iRobin93@users.noreply.github.com>
2024-11-22 13:06:59 +0100

    Add project files.

---
+++ ReverserStringen/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Skriv inn en tekst så reverserer jeg for deg!");
+string textInput = Console.ReadLine();
+reverseString(textInput);
+
+
+void reverseString(string input)
+{
+
+    string a = "";
+    for (int i = input.Length -1; i >= 0; i--)
+    {
+     a += input[i];
+    }
+
+Console.WriteLine(a);
+}
